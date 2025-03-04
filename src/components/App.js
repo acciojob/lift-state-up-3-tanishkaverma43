2bc3a07f4b7bddc0a96030952b1c7c11dcd396a7
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-03-04 17:05:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 
 import React from "react";
 import './../styles/App.css';
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionSelect = (option) => {
+      setSelectedOption(option);
+  };
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h2>Parent Component</h2>
+            <p>Selected Option: {selectedOption}</p>
+            <ChildComponent1 onSelect={handleOptionSelect} />
+            <ChildComponent2 onSelect={handleOptionSelect} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
